bc2e34cc3af9e838ad43fcaee52d4c846e6ebb3d
leidysalda <leidysalda1@gmail.com>
2020-10-09 04:47:00 +0000

    [Feature] Add file  path    => ['/bin/', '/usr/bash'],

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,7 @@
+# Sky is the limit, let's bring that limit higher
+
+exec { 'debugging':
+     command => 'sed -i "s/15/15000/" /etc/default/nginx; service nginx restart',
+     refreshonly => true,
+     path    => ['/bin/', '/usr/bash'],
+}
