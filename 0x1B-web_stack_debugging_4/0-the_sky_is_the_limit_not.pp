24031eb9ceb7bfe50564b1319b584d99f745ece6
leidysalda <leidysalda1@gmail.com>
2020-10-09 05:07:05 +0000

    [Fix] file style

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,7 +1,7 @@
 # Sky is the limit, let's bring that limit higher
 
 exec { 'debugging':
-     command => "sed -i 's/15/15000/g' /etc/default/nginx;
+     command => "sed -i 's/15/2000/g' /etc/default/nginx;
      service nginx restart",
      path    => ['/bin', '/usr/bin'],
 }
