d2a58242c6a195f4956a3b364cc31e105fde4719
leidysalda <leidysalda1@gmail.com>
2020-10-09 05:13:43 +0000

    [Fix] file 0-

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -3,5 +3,5 @@
 exec { 'debugging':
      command => "sed -i 's/15/2000/g' /etc/default/nginx;
      service nginx restart",
-     path    => ['/bin', '/usr/bin'],
+     path    => ['/usr/bin', '/bin'],
 }
