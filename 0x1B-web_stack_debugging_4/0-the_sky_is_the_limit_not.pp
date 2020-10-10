6f21893eabb97da3ab101b902593067b4475d74c
leidysalda <leidysalda1@gmail.com>
2020-10-10 03:43:31 +0000

    [Fix] Style puppet-lint 0-the_sky_is_the_limit_not.pp

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,6 +1,6 @@
 # Sky is the limit, let's bring that limit higher
 
 exec {'debugging':
-     command => 'sed -i "s/15/2000/" /etc/default/nginx; service nginx restart',
-     path    => ['/usr/bin', '/bin'],
+  command => 'sed -i "s/15/15000/" /etc/default/nginx; service nginx restart',
+  path    => ['/usr/bin', '/bin'],
 }
